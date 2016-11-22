9b8ef6cef1e66034a7c131b6df68819dde668407
RJ Madsen <rjm6823@rit.edu>
2016-11-22 00:17:04 -0500

    Adding in html and css for site mock

---
+++ AngularApp/app/js/myApp.js
@@ -0,0 +1,5 @@
+/**
+ * Created by RJ on 11/21/2016.
+ */
+
+var app = angular.module("myApp", []);
---
+++ AngularApp/app/js/myCtrl.js
@@ -0,0 +1,8 @@
+/**
+ * Created by RJ on 11/21/2016.
+ */
+
+app.controller("myCtrl", function ($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
